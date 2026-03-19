fa4a12c7b2c16eae9bb0834bccd6bffc41244d61
malligowthamsai <malligowthamsai@gmail.com>
2026-03-19 22:42:59 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const articles = [
+  {
+    title: "How to Study Effectively",
+    content: "Study daily, avoid distractions, and revise regularly."
+  },
+  {
+    title: "How to Earn Money Online",
+    content: "Freelancing, blogging, and online businesses are great ways."
+  },
+  {
+    title: "Fitness Tips for Beginners",
+    content: "Start small, stay consistent, and focus on proper diet."
+  }
+];
